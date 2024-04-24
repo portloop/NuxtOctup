d65a8027bc78e2e9d0a2273df0059f2583b43fa4
Rasim Matsko <rasimmatsko0@gmail.com>
2024-04-24 21:26:43 +0300

    Init

--- plugins/lottie-vue-player.client.js
+++
@@ -1,6 +0,0 @@
-// lottie-player.client.js
-// IMPORTANT notice the .client bit
-import * as LottiePlayer from "@dotlottie/player-component";
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(LottiePlayer);
-});
